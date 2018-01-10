581223b40f1fc3377bf3990f77300888bcb41c14
Ultimate Weapon Tong Shau Ping <32467716+highsociety2@users.noreply.github.com>
2018-01-09 22:39:59 -0500

    can now accept user move

--- first.py
+++ first.py
@@ -39,7 +39,64 @@ class Board:
     def print(self):
         print(self.matrix)
 
-myBoard = Board()
-myBoard.startingposition()
-myBoard.print()
+    def isMoveValid(self, fromRow, fromCol, toRow, toCol, player):
+        #TODO implement this
+        return True
 
+    def moveDuck(self, fromRow, fromCol, toRow, toCol, player):
+        if not self.isMoveValid(fromRow, fromCol, toRow, toCol, player):
+            return False
+        self.matrix[fromRow, fromCol] = 0
+        self.matrix[toRow, toCol] = player
+        return True
+
+
+class Game:
+    def __init__(self):
+        self.myBoard = Board()
+        self.myBoard.startingposition()
+
+    def getHumanMove(self, player):
+        possible = False
+        while not possible:
+            fromRow = int(input("Row of duck you want to move:"))
+            fromColumn = int(input("Column of duck you want to move:"))
+            if self.myBoard.matrix[fromRow, fromColumn] == player:
+                possible = True
+            else:
+                print("You don't have a duck in that square")
+        possible = False
+        while not possible:
+            toRow = int(input("Desired row you want to move that duck to:"))
+            toColumn = int(input("Desired column you want to move that duck to"))
+            if self.myBoard.matrix[toRow, toColumn] == 0:
+                possible = True
+        return fromRow, fromColumn, toRow, toColumn
+
+    def nextPlayer(self, player):
+        if player == 1:
+            return 2
+        return 1
+
+    def getRowbotMove(self, player):
+        return 0,1,0,0
+
+    def play(self):
+        human_player = int(input("do you want to be player 1 or 2?"))
+        self.myBoard.print()
+        current_player = 1
+        game_over = False
+        while not game_over:
+            if current_player == human_player:
+                fromRow, fromColumn, toRow, toColumn = self.getHumanMove(current_player)
+            else:
+                fromRow, fromColumn, toRow, toColumn = self.getRowbotMove(current_player)
+            if not self.myBoard.moveDuck(fromRow, fromColumn, toRow, toColumn, current_player):
+                print("Move was not valid")
+            else:
+                current_player = self.nextPlayer(current_player)
+                self.myBoard.print()
+
+
+myGame = Game()
+myGame.play()
